cf70006b0382538bfd7885d542ff7289f9182826
Jefferson-Butler1 <jefferson.butler@icloud.com>
2024-03-11 13:41:05 -0700

    add bootstrap

--- src/fib.php
+++ src/fib.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fibonacci Sequence</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet" crossorigin="anonymous"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3">
 </head>
 <body>
 
