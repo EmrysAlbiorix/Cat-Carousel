5f8886909aa0ccaeed51592bff6c979d9178c040
Jefferson-Butler1 <jefferson.butler@icloud.com>
2024-03-11 13:36:44 -0700

    Finish fib

--- src/fib.php
+++ src/fib.php
@@ -1,6 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fibonacci Sequence</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
 <body>
+
+<script>
+$(document).ready(function(){
+    $("#howmanysubmit").click(function() {
+        const howmany = $("#howmany").val();
+        console.log(howmany);
+        $("#howmanyinput").val(howmany);
+        $("form").submit();
+    });
+});
+</script>
+
+<div style="display:flex">
+    <form method="GET">
+        <label for="howmany">How deep do you want to go *devilface*</label>
+        <input id="howmany" type="number" name="howmanyinput" />
+        <button id="howmanysubmit" type="button">Submit</button>
+    </form>
+</div>
+
 <p>
 <?php
+if(isset($_GET["howmanyinput"])) {
     $c = 0 ;
     $a = 0 ;
 
@@ -13,7 +42,7 @@
 
     $c++ ;
 
-    while ($c <= (int)$_GET["howmany"]) {
+    while ($c <= (int)$_GET["howmanyinput"]) {
         $n = $a + $b ;
         echo "$c: $a<br>" ;
 
@@ -21,6 +50,9 @@
         $a = $b ;
         $b = $n ;
     }
+}
 ?>
 </p>
+
 </body>
+</html>
